b8fba34cc33e8a958b38e0f7cda60a8628fb05fe
Farid D. Hamzayev <Farid.Hamzayev@kapitalbank.az>
2023-07-14 11:08:11 +0400

    Initial Commit

---
+++ egg timer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  egg timer
+//
+//  Created by Farid Hamzayev on 14.07.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
